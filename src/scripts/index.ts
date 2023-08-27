fb340393547c8f7fc93bf973a06935477c5aec16
Victor Rodas <victormatiasrodas100@gmail.com>
2023-08-27 14:30:33 -0400

    import new animation

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,21 @@
-import { Gradient } from "./gradient"
+import { Gradient } from './gradient'
+import { HeroAnimation } from './random-text-animation'
 
 document.addEventListener('DOMContentLoaded', () => {
   const gradient = new Gradient()
   gradient.initGradient('#gradient-canvas')
+
+  const messengerContainer = document.querySelector('#messenger')
+
+  if (!messengerContainer) {
+    throw new Error('#messenger element does not exists')
+  }
+
+  const messages = [
+    'Este es un mensaje',
+    'este es otro mensaje',
+    'jaja...'
+  ]
+
+  new HeroAnimation(messengerContainer, messages)
 })
